902993a0c351e998091d2b1c7d8b73af177fd9ed
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2024-11-27 12:55:25 +0200

    initial settings

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import './index.css';
+import App from './components/App/App.jsx';
+import 'modern-normalize';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
