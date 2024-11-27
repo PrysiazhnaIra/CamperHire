aa575521efd9f213d4341ebd45f7ce2361a7e46e
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2024-11-27 18:16:55 +0200

    handleBtnClick for ViewNow

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import css from './Hero.module.css';
 
 export default function Hero() {
+  const navigate = useNavigate();
+
+  const handleBtnClick = () => {
+    navigate('/catalog');
+  };
   return (
     <div className={css.hero}>
       <h1 className={css.title}>Campers of your dreams</h1>
       <p className={css.text}>
         You can find everything you want in our catalog
       </p>
-      <button type="button" className={css.btn}>
+      <button type="button" className={css.btn} onClick={handleBtnClick}>
         View Now
       </button>
     </div>
