85703ac099ef9a301e1bb7a2dda9d9e1e55bcdf5
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2024-11-27 18:27:16 +0200

    not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import css from './NotFoundPage.module.css';
+
 export default function NotFoundPage() {
+  const navigate = useNavigate();
+
+  const handleBtnClick = () => {
+    navigate('/catalog');
+  };
+
   return (
-    <div>
-      <p>Page is not Found</p>
+    <div className={css.block}>
+      <h2 className={css.title}>Page is not Found</h2>
+      <button type="button" className={css.btn} onClick={handleBtnClick}>
+        Go Back
+      </button>
     </div>
   );
 }
