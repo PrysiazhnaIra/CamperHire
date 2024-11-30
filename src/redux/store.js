9e8c453d7638437bf580e38885c00e1ed282c368
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2024-11-30 17:00:01 +0200

    redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,36 @@ import { configureStore } from '@reduxjs/toolkit';
 import { campersReducer } from './camper/slice.js';
 import { filterReducer } from './filter/slice.js';
 import { favoriteReducer } from './favorite/slice.js';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'favorite',
+  version: 1,
+  storage,
+};
 
 export const store = configureStore({
   reducer: {
     campers: campersReducer,
     filters: filterReducer,
-    favorite: favoriteReducer,
+    favorite: persistReducer(persistConfig, favoriteReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
