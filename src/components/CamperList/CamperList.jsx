2d37c5d8124af1091e20890133f3a7419c42b22b
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2024-12-01 17:06:00 +0200

    logic for load more

--- src/components/CamperList/CamperList.jsx
+++ src/components/CamperList/CamperList.jsx
@@ -2,24 +2,41 @@ import { useSelector } from 'react-redux';
 import css from './CamperList.module.css';
 import { selectAllCampers } from '../../redux/camper/selectors.js';
 import CamperItem from '../CamperItem/CamperItem.jsx';
+import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function CamperList() {
   const items = useSelector(selectAllCampers);
-  const handleMore = () => {};
+  const [visibleCount, setVisibleCount] = useState(5);
+
+  const handleMore = () => {
+    if (visibleCount + 5 >= items.length) {
+      toast.success('No more campers to load!');
+    }
+    setVisibleCount(prevCount => Math.min(prevCount + 5, items.length));
+  };
+
+  const visibleItems = items.slice(0, visibleCount);
+
   return (
     <div>
-      {items.length > 0 && (
+      <Toaster position="center" reverseOrder={false} />
+      {items.length > 0 ? (
         <ul className={css.list}>
-          {items.map(item => {
+          {visibleItems.map(item => {
             return <CamperItem key={item.id} item={item} />;
           })}
         </ul>
+      ) : (
+        <p>Nothing was found for your request. Please try again.</p>
       )}
-      <div className={css.btnBlock}>
-        <button type="button" className={css.btn} onClick={handleMore}>
-          Load more
-        </button>
-      </div>
+      {visibleCount < items.length && (
+        <div className={css.btnBlock}>
+          <button type="button" className={css.btn} onClick={handleMore}>
+            Load more
+          </button>
+        </div>
+      )}
     </div>
   );
 }
